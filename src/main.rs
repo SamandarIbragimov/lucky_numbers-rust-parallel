8c6d9319746801b83dcd2b7f326e5e942989dfaa
Samandar Ibragimov <samandar0406@gmail.com>
2023-11-12 15:49:09 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,11 @@
 extern crate rayon;
 use rayon::prelude::*;
 
-fn lucky_numbers(matrix: Vec<Vec<u32>>) -> Vec<u32> {
-    let r_min:Vec<u32> = matrix.iter().map(|r| *r.iter().min().unwrap()).collect();
-    let c_max:Vec<u32> = (0..matrix[0].len()).into_iter().map(|j| matrix.iter().map(|r| r[j]).max().unwrap()).collect();
+fn lucky_numbers(matrix: Vec<Vec<i32>>) -> Vec<i32> {
+    let r_min:Vec<i32> = matrix.iter().map(|r| *r.iter().min().unwrap()).collect();
+    let c_max:Vec<i32> = (0..matrix[0].len()).into_iter().map(|j| matrix.iter().map(|r| r[j]).max().unwrap()).collect();
     
-    let mut lucky = Vec::new();
-    
-    for i in 0..matrix.len() {
-        for j in 0..matrix[i].len() {
-            if matrix[i][j] == r_min[i] && matrix[i][j] == c_max[j] {
-                lucky.push(matrix[i][j]);
-            }
-        }
-    }
-    
-    lucky
-}
-
- fn par_lucky_numbers(matrix: Vec<Vec<u32>>) -> Vec<u32> {
-    let r_min: Vec<u32> = matrix.par_iter()
-        .map(|r| *r.iter().min().unwrap())
-        .collect();
-    
-    let c_max: Vec<u32> = (0..matrix[0].len()).into_par_iter()
-        .map(|j| matrix.iter().map(|r| r[j]).max().unwrap())
-        .collect();
-
-        let lucky: Vec<u32> = matrix.par_iter()
+    matrix.iter()
         .enumerate()
         .flat_map(|(i, r)| {
             r.iter()
@@ -41,18 +19,42 @@ fn lucky_numbers(matrix: Vec<Vec<u32>>) -> Vec<u32> {
                 })
                 .collect::<Vec<_>>() 
         })
-        .collect();
-
-    
-    lucky
+        .collect()
 }
 
-fn generate_matrix(rows: usize, columns: usize) -> Vec<Vec<u32>> {
-    (0..rows).map(|_| (0..columns).map(|_| rand::random::<u32>() % 10 + 1).collect()).collect()
+ fn par_lucky_numbers(matrix: Vec<Vec<i32>>) -> Vec<i32> {
+    let r_min: Vec<i32> = matrix.par_iter()
+        .map(|r| *r.iter().min().unwrap())
+        .collect();
+    
+    let c_max: Vec<i32> = (0..matrix[0].len()).into_par_iter()
+        .map(|j| matrix.iter().map(|r| r[j]).max().unwrap())
+        .collect();
+
+        matrix.par_iter()
+        .enumerate()
+        .flat_map(|(i, r)| {
+            r.iter()
+                .enumerate()
+                .filter_map(|(j, &e)| {
+                    if e == r_min[i] && e == c_max[j] {
+                        Some(e)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>() 
+        })
+        .collect()
+
+}
+
+fn generate_matrix(rows: i32, columns: i32) -> Vec<Vec<i32>> {
+    (0..rows).map(|_| (0..columns).map(|_| rand::random::<i32>() % 100000).collect()).collect()
 }
  
 fn main() {
-    let matrix = generate_matrix(5,5);
+    let matrix = generate_matrix(100,100);
     for row in &matrix {
         println!("{:?}", row);
     }
